224199603e220a5be2a7527eb4b23c4ea155ecc9
yunsungLEE <chance07236@naver.com>
2025-02-06 15:46:28 +0900

    chore: 인터셉터 오류 & 오어스 오류 수정중...

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { API_PATHS } from '@/constants/apiPath';
-// import store from '@/redux/store';
-// import { logout } from '@/redux/authReducers';
+import store from '@/redux/store';
+import { logout } from '@/redux/authReducers';
 
 const {
-  AUTH: { REFRESH_TOKEN },
+  AUTH: { REFRESH_TOKEN, SIGNOUT },
 } = API_PATHS;
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -19,6 +19,7 @@ const instance = axios.create({
 
 // 무한 루프 예방 변수
 let isRefreshing = false;
+const logoutInitiatedKey = 'logoutInitiated';
 
 instance.interceptors.response.use(
   (response) => response,
@@ -35,24 +36,35 @@ instance.interceptors.response.use(
       isRefreshing = true; // 무한 루프 예방
 
       try {
+        console.log('🔄 리프레시 토큰 요청 중...');
         await instance.post(REFRESH_TOKEN);
+        console.log('✅ 리프레시 토큰 갱신 성공');
         isRefreshing = false;
         return instance(originalRequest); // 실패한 요청 재시도
-      } catch (error: unknown) {
-        if (axios.isAxiosError(error)) {
-          console.error(error.response?.data.message);
-        } else if (error instanceof Error) {
-          console.error(error.message);
-        } else {
-          console.error('Unexpected error:', error);
+      } catch (refreshError: unknown) {
+        // console.error('❌ 리프레시 토큰 갱신 실패', error);
+
+        if (!localStorage.getItem(logoutInitiatedKey)) {
+          localStorage.setItem(logoutInitiatedKey, 'true'); // 플래그 설정
+
+          // Case 2: 리프레시 토큰 만료 -> 로그아웃 처리
+          try {
+            await instance.post(SIGNOUT); // Attempt to sign out
+            console.log('✅ SIGNOUT successful');
+          } catch (signoutError: unknown) {
+            console.warn(
+              'SIGNOUT failed with error',
+              signoutError.response?.status,
+            );
+          } finally {
+            // Always execute this block, regardless of SIGNOUT success or failure
+            store.dispatch(logout()); // Clear global user information
+            alert('세션이 만료되었습니다. 다시 로그인 해주세요.');
+            window.location.href = '/start';
+          }
         }
 
-        // Case 2: 리프레시 토큰 만료 -> 로그아웃 처리
-        // await instance.post(SIGNOUT);
-        // alert('세션이 만료되었습니다.');
-        // store.dispatch(logout()); // ✅ 전역객체에도 유저 정보 초기화
-        // window.location.href = '/start';
-        // return Promise.reject(error);
+        return Promise.reject(refreshError);
       }
     }
 
