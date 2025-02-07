cbd3e978bd7c679550b4b01ca4b84dbda30291b0
Dhani5703 <dems9811@naver.com>
2025-02-07 14:37:10 +0900

    feat: 모임 목록 페이지 헤더 추가

--- src/pages/meetings/index.tsx
+++ src/pages/meetings/index.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-import axios from '@/api/axiosInstance';
+import instance from '@/api/axiosInstance';
+import Header from '@/components/shared/layout/Header';
 import CardList from '@/components/meetings/card/CardList';
 import { Meeting } from '@/components/meetings/card/Card';
 
 const MeetingPage: React.FC = () => {
   const [meetings, setMeetings] = useState<Meeting[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchMeetings = async () => {
@@ -13,14 +13,12 @@ const MeetingPage: React.FC = () => {
         const cookie = document.cookie || ''; // 브라우저 환경에서 쿠키 가져오기
         const isLoggedIn = cookie.includes('accessToken'); // 로그인 여부 판단
 
-        const response = await axios.get('/api/meetings', {
+        const response = await instance.get('/api/meetings', {
           withCredentials: isLoggedIn,
         });
         setMeetings(response.data.data.meetingList);
       } catch (error) {
         console.error('모임 목록 불러오기 실패', error);
-      } finally {
-        setLoading(false);
       }
     };
     fetchMeetings();
@@ -28,7 +26,8 @@ const MeetingPage: React.FC = () => {
 
   return (
     <div className="mx-auto max-w-2xl p-4">
-      {loading ? <p>Loading...</p> : <CardList meetings={meetings} />}
+      <Header title="모임 목록" />
+      <CardList meetings={meetings} />
     </div>
   );
 };
