efda7a3466349f69d6ddb263f799c88f669366ef
yunsungLEE <chance07236@naver.com>
2025-02-07 15:04:45 +0900

    restructure: GNB 컴포넌트 위치 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import Head from 'next/head';
-import GNB from '@/components/shared/layout/GNB';
+import GNB from '@/components/gnb';
 import Provider from '@/components/provider';
 import '@/styles/globals.css';
 
