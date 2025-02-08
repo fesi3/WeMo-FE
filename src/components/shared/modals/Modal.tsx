732528fa6260d5c98600ec06042954ccbe750e2c
yunsungLEE <chance07236@naver.com>
2025-02-08 15:17:22 +0900

    design: modalBackDrop wrapper로 변경후, opacity의 child 영향으로 인한 bg-opacity로 수정

--- src/components/shared/modals/Modal.tsx
+++ src/components/shared/modals/Modal.tsx
@@ -22,25 +22,26 @@ export default function Modal({
   return (
     isOpen && (
       <ModalPortal>
-        <ModalBackDrop isOpen={isOpen} handleClose={handleClose} />
-        <div
-          className={twMerge(
-            'fixed left-1/2 top-1/2 z-[11] flex w-full max-w-screen-md -translate-x-1/2 -translate-y-1/2 p-6',
-            className,
-          )}
-        >
-          <div className="flex w-full flex-col rounded-lg bg-white p-6 text-black">
-            <div className="flex justify-end pb-5">
-              {title ? (
-                <span className="grow text-xl font-semibold">{title}</span>
-              ) : null}
-              <button onClick={handleClose}>
-                <XMarkIcon className="w-8 opacity-50" />
-              </button>
+        <ModalBackDrop isOpen={isOpen} handleClose={handleClose}>
+          <div
+            className={twMerge(
+              'fixed left-1/2 top-1/2 z-[12] flex w-full max-w-screen-md -translate-x-1/2 -translate-y-1/2 p-6',
+              className,
+            )}
+          >
+            <div className="flex w-full flex-col rounded-lg bg-white p-6 text-black">
+              <div className="flex justify-end pb-5">
+                {title ? (
+                  <span className="grow text-xl font-semibold">{title}</span>
+                ) : null}
+                <button onClick={handleClose}>
+                  <XMarkIcon className="w-8 opacity-50" />
+                </button>
+              </div>
+              <div className="h-full w-full">{children}</div>
             </div>
-            <div className="h-full w-full">{children}</div>
           </div>
-        </div>
+        </ModalBackDrop>
       </ModalPortal>
     )
   );
