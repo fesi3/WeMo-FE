113bca3f4813573c0e0e65b49fe795c998acf3b1
kimsayhi <kimse418@gmail.com>
2025-02-08 23:50:46 +0900

    feat: SSR환경에서 쿠키를 사용할 수 있도록 instnace 생성

--- src/pages/plans/[id].tsx
+++ src/pages/plans/[id].tsx
@@ -1,4 +1,4 @@
-import { fetchPlanDetail } from '@/api/plan';
+import { fetchPlanDetailSSR } from '@/api/ssr/plans';
 import PlanDetailMain from '@/components/planDetail/PlanDetailMain';
 import { QUERY_KEY } from '@/constants/queryKey';
 
@@ -12,12 +12,13 @@ import { GetServerSideProps } from 'next';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { id } = context.query;
+  //쿠키를 추출해 SSR전용 패치함수에 전달합니다.
   const cookie = context.req.headers.cookie || '';
   const queryClient = new QueryClient();
   const idNum = parseInt(id as string);
   await queryClient.prefetchQuery({
     queryKey: QUERY_KEY.planDetail(idNum),
-    queryFn: () => fetchPlanDetail(idNum, cookie),
+    queryFn: () => fetchPlanDetailSSR(idNum, cookie),
   });
 
   return {
