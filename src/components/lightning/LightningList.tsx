f3b3d930c303edcb2fc567c48070c44d9e9a31cf
김민규 <mingyu@MinQs-MacBook-Pro.local>
2025-02-10 04:07:56 +0900

    feat: 모임 참여 버튼 생성

--- src/components/lightning/LightningList.tsx
+++ src/components/lightning/LightningList.tsx
@@ -1,10 +1,23 @@
 import { LightningMeetup } from '@/types/lightningType';
+import axiosInstance from '@/api/axiosInstance';
 
 interface LightningListProps {
   meetups: LightningMeetup[];
 }
 
 const LightningList = ({ meetups }: LightningListProps) => {
+  const handleParticipation = async (lightningId: number) => {
+    try {
+      const response = await axiosInstance.post(
+        `/api/lightnings/participation/${lightningId}`,
+      );
+      alert('참여가 완료되었습니다!');
+      console.log('참여 성공:', response.data);
+    } catch (error) {
+      console.error('참여 실패:', error);
+      alert('참여 요청 중 오류가 발생했습니다.');
+    }
+  };
   return (
     <div className="mx-auto h-[400px] w-full max-w-[1200px] overflow-y-auto rounded-b-xl bg-white p-4">
       {meetups.map((meetup) => (
@@ -28,15 +41,29 @@ const LightningList = ({ meetups }: LightningListProps) => {
           </p>
 
           {/* 위치 및 시간 */}
-          <div className="mt-2 flex items-center justify-between text-sm text-gray-500">
-            <span className="truncate">{meetup.address}</span>
-            <span className="font-semibold">
-              {new Date(meetup.lightningDate).toLocaleTimeString('ko-KR', {
-                hour: '2-digit',
-                minute: '2-digit',
-              })}
-            </span>
+          <div className="mt-2 flex items-center text-sm text-gray-500">
+            <div>
+              <span className="truncate">{meetup.address}</span>
+              <span className="ml-4 font-semibold">
+                {new Date(meetup.lightningDate).toLocaleTimeString('ko-KR', {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })}
+              </span>
+              <div>
+                <span className="font-semibold">
+                  {meetup.lightningParticipants}/{meetup.lightningCapacity}
+                </span>
+                명 참여 예정
+              </div>
+            </div>
           </div>
+          <button
+            onClick={() => handleParticipation(meetup.lightningId)}
+            className="mt-2 w-full rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+          >
+            참여하기
+          </button>
         </div>
       ))}
     </div>
