6589be5be13e533faf2d8457931bfcabb0a21eeb
yunsungLEE <chance07236@naver.com>
2025-02-09 13:45:57 +0900

    type: PlanListResponse, PlanListData -> plan.ts 파일로 이동

--- src/components/gnb/header/index.tsx
+++ src/components/gnb/header/index.tsx
@@ -8,18 +8,51 @@ import Search from '@/components/search';
 import SearchModal from '@/components/search/searchModal';
 import useToggle from '@/hooks/useToggle';
 import SearchContents from '@/components/search/searchContents';
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { API_PATHS } from '@/constants/apiPath';
+import instance from '@/api/axiosInstance';
 
 // GNB 레이아웃 컴포넌트에서 렌더링 되는 header 컴포넌트입니다.
 // 페이지마다 출력이 달라 path를 조회해 조건부 렌더링 합니다.
 // 로그인 여부를 전역객체에서 조회해 조건부 렌더링 합니다.
 // 상위 컴포넌트로 부터 유저 정보 응답을 내려받아 라우팅 합니다.
 function GNBHeader() {
-  const router = useRouter();
-  const showGnbHeader = hideGnbHeaderRoutes.includes(router.pathname);
   const { toggleValue, handleOpen, handleClose } = useToggle();
-  const searchHandleClick = () => {
+  const searchParams = useSearchParams();
+  const searchKeyword = searchParams.get('q');
+  const encodedSearchKeyWord: string = searchKeyword
+    ? encodeURIComponent(searchKeyword)
+    : '';
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const {
+    PLAN: { GET_ALL },
+  } = API_PATHS;
+
+  const showGnbHeader = hideGnbHeaderRoutes.includes(router.pathname);
+  const handleSearchBarClick = () => {
     handleOpen();
   };
+
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    router.push(`${router.pathname}/?q=${e.target.value}`);
+  };
+
+  const query = useQuery({
+    queryKey: ['searchKeyword'],
+    queryFn: async () => {
+      return await instance.get(`${GET_ALL(`query=${encodedSearchKeyWord}`)}`);
+    },
+    gcTime: 0,
+    staleTime: 0,
+  });
+
+  useEffect(() => {
+    queryClient.invalidateQueries({ queryKey: ['searchKeyword'] });
+    console.log(query.data?.data.data);
+  }, [searchKeyword]);
 
   return (
     <div className="flex max-h-full w-full flex-col">
@@ -32,7 +65,7 @@ function GNBHeader() {
                 <Image width={70} height={0} src={logo} alt="wemo-gnb-logo" />
               </Link>
               {/* 검색바 */}
-              <Search searchHandleClick={searchHandleClick} />
+              <Search handleSearchBarClick={handleSearchBarClick} />
               {/* 알림 */}
               <div onClick={() => alert('구현 예정입니다.')}>
                 <GNBItem name={'알림'} isHeader isRouteDisabled />
@@ -43,12 +76,16 @@ function GNBHeader() {
           {/* 헤더 공간 확보 */}
           <div className="invisible h-14" />
 
+          {/* 검색 모달 출력 */}
           <SearchModal
             isOpen={toggleValue}
             handleClose={handleClose}
             className="flex h-screen flex-col gap-[38px] px-[30px] pb-[119px] pt-28 md:px-[65px] md:pt-[188px] lg:max-w-none lg:items-center"
           >
-            <SearchContents />
+            <SearchContents
+              searchKeyword={searchKeyword}
+              handleSearchInputChange={handleSearchInputChange}
+            />
           </SearchModal>
         </>
       )}
--- src/components/search/searchContents.tsx
+++ src/components/search/searchContents.tsx
@@ -1,6 +1,19 @@
+import { useQueryClient } from '@tanstack/react-query';
 import Input from '../shared/input';
+import { PlanListResponse } from '@/types/plans';
 
-function SearchContents() {
+interface SearchContentsProps {
+  handleSearchInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  searchKeyword: string | null;
+}
+
+function SearchContents({
+  handleSearchInputChange,
+  searchKeyword,
+}: SearchContentsProps) {
+  const queryClient = useQueryClient();
+  const data = queryClient.getQueryData<PlanListResponse>(['searchKeyword']);
+  console.log(data, '---searchResult---');
   return (
     <div className="flex max-h-full w-full flex-col lg:items-center">
       {/* 서치 바  */}
@@ -9,12 +22,15 @@ function SearchContents() {
         placeholder={'찾고 싶은 모임을 검색해 보세요!'}
         className="h-12 flex-grow-0 rounded-[20px] lg:w-[724px]"
         onClick={(e) => e.stopPropagation()}
+        onChange={handleSearchInputChange}
         autoFocus
       />
       {/* 안내 텍스트  */}
       <div className="mt-[38px] flex w-full flex-col items-center md:items-start lg:w-[724px]">
         <span className="text- font-semibold text-primary-100">
-          {'모임에 대한 검색 결과'}
+          {searchKeyword
+            ? `"${searchKeyword}"에 대한 검색 결과`
+            : `"${searchKeyword}"에 대한 검색 결과가 없습니다.`}
         </span>
         {/* 검색 결과 카드 리스트 */}
         <div
