2840cdabbe4f87c68d023b1cc499840712aadd97
Dhani5703 <dems9811@naver.com>
2025-02-09 18:40:33 +0900

    feat: 모임 목록 페이지 쿠키 없이 데이터 로딩 (비회원에게 401 에러 나지 않도록 수정)

--- src/api/fetchMeetings.ts
+++ src/api/fetchMeetings.ts
@@ -1,6 +1,7 @@
 import instance from '@/api/axiosInstance';
 import { QueryFunctionContext } from '@tanstack/react-query';
 import { FetchMeetingsResponse } from '@/types/api/meetingList';
+import { AxiosError } from 'axios';
 
 export const fetchMeetings = async ({
   pageParam = null,
@@ -10,15 +11,25 @@ export const fetchMeetings = async ({
 >): Promise<FetchMeetingsResponse> => {
   const [, selectedSort, selectedCategory] = queryKey;
 
-  const response = await instance.get('/api/meetings', {
-    params: {
-      sort: selectedSort, // 정렬 기준 추가
-      category: selectedCategory, // 카테고리 추가
-      cursor: pageParam,
-      size: 10,
-    },
-    withCredentials: true,
-  });
+  try {
+    const response = await instance.get('/api/meetings', {
+      params: {
+        sort: selectedSort, // 정렬 기준 추가
+        category: selectedCategory, // 카테고리 추가
+        cursor: pageParam,
+        size: 10,
+      },
+      withCredentials: false,
+    });
 
-  return response.data.data;
+    return response.data.data;
+  } catch (error) {
+    const axiosError = error as AxiosError;
+
+    if (axiosError.response?.status === 401) {
+      console.warn('비로그인 상태에서 401 에러 발생, 기본 데이터를 반환.');
+      return { meetingList: [], nextCursor: null }; // 빈 데이터 반환
+    }
+    throw axiosError; // 다른 에러는 그대로 던짐
+  }
 };
