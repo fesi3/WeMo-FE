29cbd38d4ed858d2192d7365e87ebb0a49d11b4b
G1Coding <32190250@dankook.ac.kr>
2025-02-10 16:30:33 +0900

    feat: 마이페이지 api 연동

--- src/pages/user/[username]/calendar.tsx
+++ src/pages/user/[username]/calendar.tsx
@@ -2,93 +2,48 @@ import React, { useCallback, useState } from 'react';
 import {
   getDateInfo,
   getFilteredSchedulesByMonth,
-  parseDateString,
+  getFirstAndLastDayOfMonth,
 } from '@/utils/dateUtils';
 import ScheduleTitle from '@/components/mypage/calendar/schedule/ScheduleTitle';
 import ScheduleList from '@/components/mypage/calendar/schedule/ScheduleList';
 import MyPlanCalendar from '@/components/mypage/calendar/MyPlanCalendar';
-import { StaticImageData } from 'next/image';
-import temporaryImg from '@/assets/images/Rectangle 6188.png';
+import { useMyPlanCalendar } from '@/hooks/mypage/fetch/useMypageData';
 
 type ValuePiece = Date | null;
 export type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-export interface SchedulePlanProps {
-  planId: number;
-  planName: string;
-  planImagePath: string | StaticImageData;
-  dateTime: string | Date; // 서버에서는 string으로 응답줌
-  category: string;
-  addressDetail: string;
-  isOpen: boolean; // 개설 확정 상태
-  isCompelete: boolean; // 이용 완료 상태
-}
-
-// interface CalendarPageProps {
-//   serverPlansData: SchedulePlanProps[];
-// }
-
-const serverPlansData: SchedulePlanProps[] = [
-  {
-    planId: 1,
-    planName: '노래방',
-    dateTime: '2025-02-10',
-    isOpen: true,
-    isCompelete: false,
-    category: '달램핏',
-    planImagePath: temporaryImg,
-    addressDetail: '강남구',
-  },
-  {
-    planId: 12,
-    planName: '운동',
-    dateTime: '2025-02-11',
-    isOpen: true,
-    isCompelete: false,
-    category: '달램핏',
-    planImagePath: temporaryImg,
-    addressDetail: '마포구',
-  },
-  {
-    planId: 2,
-    planName: '축구',
-    dateTime: '2025-02-01',
-    isOpen: true,
-    isCompelete: true,
-    category: '달램핏',
-    planImagePath: temporaryImg,
-    addressDetail: '종로구',
-  },
-];
-
-// export default function CalendarPage( props : InferGetServerSidePropsType<typeof getServerSideProps>) {
 export default function CalendarPage() {
-  // console.log(props.calendarPlan);
   const today = new Date();
   const [selectedDate, setSelectedDate] = useState<Value>(today); // 선택한 날짜 상태 (초기화: 오늘날짜)
-
-  // 일정 날짜를 string -> date 객체로 변환
-  const parsedPlansDate = serverPlansData.map((plan) => ({
-    ...plan,
-    date: parseDateString(plan.dateTime),
-  }));
 
   const handleDateChange = useCallback((newDate: Value) => {
     setSelectedDate(newDate);
   }, []);
 
-  // 현재 날짜의 연,월,일 구하기 - 유틸 함수로 분리
+  // 현재 날짜의 연,월,일
   const currentDate = getDateInfo();
-  console.log('현재 날짜', currentDate);
 
-  // 일정 필터링 - 이달의 일정
-  const filteredSchedulesInThisMonth = getFilteredSchedulesByMonth(
-    parsedPlansDate,
-    `${currentDate.year}-${currentDate.month}`,
+  // 특정 월의 첫째 - 마지막 날 가져오기!
+  const { startDate, endDate } = getFirstAndLastDayOfMonth(
+    currentDate.year,
+    currentDate.month,
   );
 
-  console.log('이달의 일정', filteredSchedulesInThisMonth);
+  const { data, isLoading, error } = useMyPlanCalendar(startDate, endDate);
 
+  if (isLoading) return <div>달력 로딩 중...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  if (!data) return <div>일정이 없습니다!</div>;
+
+  const myCalendarPlanData = data?.data.planList;
+
+  console.log('서버에서 온 데이터', myCalendarPlanData);
+
+  // // 일정 필터링 - 이달의 일정
+  const filteredSchedulesInThisMonth = getFilteredSchedulesByMonth(
+    myCalendarPlanData,
+    `${currentDate.year}-${currentDate.month}`,
+  );
   //이달의 일정 추출(하트 표시 위함)
   const heartsPlan = [
     ...new Set(
@@ -97,7 +52,6 @@ export default function CalendarPage() {
       ),
     ),
   ];
-  console.log('중복 제거한 이달의 일정이 있는 날짜 반환', heartsPlan);
 
   // 선택한 날짜 = 이달의 일정 배열에서의 날짜 -> 일정 리스트를 렌더링
   const renderPlanList = filteredSchedulesInThisMonth.filter((plan) => {
@@ -105,11 +59,6 @@ export default function CalendarPage() {
     const selectedDateInfo = getDateInfo(selectedDate as Date); // 선택한 날짜
     return planDateInfo.day === selectedDateInfo.day;
   });
-
-  console.log(
-    `선택한 날짜에 있는 일정? ${renderPlanList.length} 개`,
-    renderPlanList,
-  );
 
   return (
     <div>
@@ -126,13 +75,11 @@ export default function CalendarPage() {
           currentDate={currentDate}
         />
 
-        {renderPlanList && renderPlanList.length > 0 ? (
-          renderPlanList.map((plan) => (
-            <ScheduleList key={plan.planId} renderPlanListData={plan} />
-          ))
-        ) : (
-          <p className="text-sm text-gray-500">일정이 없습니다.</p>
-        )}
+        {renderPlanList && renderPlanList.length > 0
+          ? renderPlanList.map((plan) => (
+              <ScheduleList key={plan.planId} renderPlanListData={plan} />
+            ))
+          : null}
       </div>
     </div>
   );
