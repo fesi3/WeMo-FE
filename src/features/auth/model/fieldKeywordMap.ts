8d4bb86a96170ea7000a692c5682e02675624798
yunsungLEE <chance07236@naver.com>
2025-04-08 16:57:11 +0900

    fix: 회원가입 실패 시, 서버 에러 메세지 출력 에러처리

---
+++ src/features/auth/model/fieldKeywordMap.ts
@@ -0,0 +1,7 @@
+export const fieldKeywordMap: [string, string][] = [
+  ['이메일', 'email'],
+  ['닉네임', 'nickname'],
+  ['회사명', 'companyName'],
+  ['비밀번호', 'password'],
+  ['비밀번호 확인', 'passwordCheck'],
+];
