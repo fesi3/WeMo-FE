83352419b158a50a839ebe14bc55178d1df14e5d
yunsungLEE <chance07236@naver.com>
2025-04-08 17:12:54 +0900

    fix: 회원가입 헤더 간격 수정

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -4,8 +4,8 @@ import Header from '@/widgets/Header';
 export function Register() {
   return (
     <>
-      <Header title="회원목록" />
-      <div className="mt-[22.5px] flex w-full justify-center md:mt-[80px]">
+      <Header title="회원가입" />
+      <div className="mt-[80px] flex w-full justify-center">
         <SignupForm />
       </div>
     </>
