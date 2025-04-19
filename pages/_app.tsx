27c700f33972b826f84839731513af8d960acf23
yunsungLEE <chance07236@naver.com>
2025-04-19 13:31:22 +0900

    chore: GNB import 문 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 import Provider from '@/app/providers';
-import GNB from '@/widgets/GNB';
+import GNB from '@/widgets/GNB/index';
 import './styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
