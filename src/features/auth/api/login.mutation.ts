014ba210d6eceae183f939d5041f95173d0f11ec
yunsungLEE <chance07236@naver.com>
2025-05-06 14:09:14 +0900

    refac: login 요청 fetchData -> axiosInstance 변경

--- src/features/auth/api/login.mutation.ts
+++ src/features/auth/api/login.mutation.ts
@@ -8,7 +8,7 @@ import { LoginFormTypes } from '@/features/auth/model/type';
 import { API_PATHS } from '@/shared/constants/apiPath';
 import { loginErrorType } from '@/features/auth/model/login.validation';
 import { login } from '@/shared/lib/redux/authReducers';
-import fetchData from '@/shared/api/fetchData';
+import axiosInstance from '@/shared/utils/axios';
 
 const {
   AUTH: { SIGNIN },
@@ -25,12 +25,14 @@ function useLoginMutaion({ loginFormValue, setErrors }: useLoginMutaionProps) {
   const queryClient = useQueryClient();
 
   return useMutation<LoginFormTypes, AxiosError<{ message: string }>>({
-    mutationFn: () =>
-      fetchData({
-        param: SIGNIN,
+    mutationFn: async () => {
+      const res = await axiosInstance({
         method: 'post',
-        requestData: loginFormValue,
-      }),
+        url: SIGNIN,
+        data: loginFormValue,
+      });
+      return res.data;
+    },
     onSuccess: () => {
       // 로그인이 성공하면 로그인 여부를 상태 업데이트 및 쿼리 invalidate
       // (invalidate 안해주면 요청 GNB 렌더링 되도 요청 안보냄)
